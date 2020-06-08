9b4c9c62bbe660ec6724054dbaa541f992595a0b
tylergreason <tyler.greason@protonmail.com>
2020-06-08 15:15:13 -0400

    Added redux store folders and Title and Traits components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Title from './components/Title'
+import Traits from './components/Traits'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         This is my character generator for Knave! 
+        <Title title='Knave Character Generator'></Title>
+        <Traits></Traits>
     </div>
   );
 }
---
+++ src/components/Traits.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+class Traits extends Component {
+    render(){
+        return(
+            <>
+            
+            </>
+        )
+    }
+}
+
+export default Traits
